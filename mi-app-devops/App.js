1613953cba5ea5a4488dc3b298311acdd2190ac9
ducuaradavid <ducuaradavid4@gmail.com>
2026-06-18 17:02:32 -0500

    Mision 3: Mensaje de mantenimineto

--- mi-app-devops/App.js
+++ mi-app-devops/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo</Text>
-      <StatusBar style="auto" />
+      <View style={styles.card}>
+        <Text style={styles.warningTitle}>⚠️ Sistema en Mantenimiento</Text>
+        <Text style={styles.warningText}>
+          Estamos realizando mejoras en la plataforma DevOps. Por favor, intenta más tarde.
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: '#7f1d1d', justifyContent: 'center', alignItems: 'center', padding: 20 },
+  card: { backgroundColor: '#ffffff', padding: 30, borderRadius: 15, alignItems: 'center', shadowBlur: 10, elevation: 5 },
+  warningTitle: { fontSize: 20, fontWeight: 'bold', color: '#b91c1c', marginBottom: 10, textAlign: 'center' },
+  warningText: { fontSize: 14, color: '#4b5563', textAlign: 'center' }
 });
